f143d3bef628c0c5b3ae4e5d7aad3d8b174eaa47
Medo <martinmederly@gmail.com>
2023-12-03 08:09:55 +0100

    feat: day 3 part 2

--- day3/day3.go
+++ day3/day3.go
@@ -6,15 +6,21 @@ import (
 )
 
 const RUNE_DOT = rune('.')
+const RUNE_STAR = rune('*')
 const RUNE_DIGIT_START = rune('0')
 const RUNE_DIGIT_END = rune('9')
 
 type SchemaRow = []rune
 type Schema = []SchemaRow
 type PartNumber struct {
+	id              int
 	number          int
 	adjacentSymbols []rune
 }
+type Gear struct {
+	adjacentNumberIds []int
+}
+type GearMap map[string]*Gear
 
 func Solve(input string) {
 
@@ -31,9 +37,14 @@ func Solve(input string) {
 
 	// ========== part 1
 	numbers := []PartNumber{}
+	gearMap := GearMap{}
+	numId := 0
+
 	for y, _ := range schema {
 		isNumber := false
+		numId++
 		num := PartNumber{
+			id:              numId,
 			adjacentSymbols: make([]rune, 0),
 		}
 		for x, c := range schema[y] {
@@ -44,47 +55,33 @@ func Solve(input string) {
 					isNumber = true
 					num.number = runeNumber(c)
 
-					if x > 0 {
-						if y > 0 && isSymbol(schema[y-1][x-1]) {
-							num.adjacentSymbols = append(num.adjacentSymbols, schema[y-1][x-1])
-						}
-						if y < len(schema)-1 && isSymbol(schema[y+1][x-1]) {
-							num.adjacentSymbols = append(num.adjacentSymbols, schema[y+1][x-1])
-						}
-						if isSymbol(schema[y][x-1]) {
-							num.adjacentSymbols = append(num.adjacentSymbols, schema[y][x-1])
-						}
-					}
-					if y > 0 && isSymbol(schema[y-1][x]) {
-						num.adjacentSymbols = append(num.adjacentSymbols, schema[y-1][x])
-					}
-					if y < len(schema)-1 && isSymbol(schema[y+1][x]) {
-						num.adjacentSymbols = append(num.adjacentSymbols, schema[y+1][x])
-					}
+					checkSymbol(schema, y-1, x-1, &num, numId, &gearMap)
+					checkSymbol(schema, y, x-1, &num, numId, &gearMap)
+					checkSymbol(schema, y+1, x-1, &num, numId, &gearMap)
+
+					checkSymbol(schema, y-1, x, &num, numId, &gearMap)
+					checkSymbol(schema, y+1, x, &num, numId, &gearMap)
+
 				}
 
-				if x < len(schema[y])-1 {
-					if y > 0 && isSymbol(schema[y-1][x+1]) {
-						num.adjacentSymbols = append(num.adjacentSymbols, schema[y-1][x+1])
-					}
-					if y < len(schema)-1 && isSymbol(schema[y+1][x+1]) {
-						num.adjacentSymbols = append(num.adjacentSymbols, schema[y+1][x+1])
-					}
-					if isSymbol(schema[y][x+1]) {
-						num.adjacentSymbols = append(num.adjacentSymbols, schema[y][x+1])
-					}
-				}
+				checkSymbol(schema, y-1, x+1, &num, numId, &gearMap)
+				checkSymbol(schema, y, x+1, &num, numId, &gearMap)
+				checkSymbol(schema, y+1, x+1, &num, numId, &gearMap)
 			} else if isNumber {
 				isNumber = false
 				numbers = append(numbers, num)
+				numId++
 				num = PartNumber{
+					id:              numId,
 					adjacentSymbols: make([]rune, 0),
 				}
 			}
 		}
 		if isNumber {
 			numbers = append(numbers, num)
+			numId++
 			num = PartNumber{
+				id:              numId,
 				adjacentSymbols: make([]rune, 0),
 			}
 		}
@@ -100,10 +97,56 @@ func Solve(input string) {
 
 	fmt.Println(numbers)
 	fmt.Println("Part 1:", part1)
+
+	part2 := 0
+	for k, g := range gearMap {
+		if len(g.adjacentNumberIds) == 2 {
+			fmt.Print("Gear:", k, " => ")
+			gearRatio := 1
+			for _, gearNumId := range g.adjacentNumberIds {
+				for _, num := range numbers {
+					if num.id == gearNumId {
+						gearRatio *= num.number
+					}
+				}
+			}
+			fmt.Print("Gear ratio = ", gearRatio)
+			part2 += gearRatio
+		}
+		fmt.Println()
+	}
+
+	fmt.Println("Part 2:", part2)
 }
 
 func runeNumber(r rune) int {
 	return int(r - RUNE_DIGIT_START)
+}
+
+func checkSymbol(schema Schema, y int, x int, partNum *PartNumber, numId int, gearMap *GearMap) {
+	if x < 0 || y < 0 || y >= len(schema) || x >= len(schema[0]) {
+		return
+	}
+
+	c := schema[y][x]
+
+	if isSymbol(c) {
+		partNum.adjacentSymbols = append(partNum.adjacentSymbols, schema[y][x])
+	}
+
+	if c == RUNE_STAR {
+		key := fmt.Sprintf("%d-%d", x, y)
+		gear, exists := (*gearMap)[key]
+
+		if exists {
+			gear.adjacentNumberIds = append(gear.adjacentNumberIds, numId)
+		} else {
+			gear = &Gear{
+				adjacentNumberIds: []int{numId},
+			}
+			(*gearMap)[key] = gear
+		}
+	}
 }
 
 func isSymbol(r rune) bool {

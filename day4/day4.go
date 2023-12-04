186351d9e7d5a0c06f8dabb9ac0999c940983bc8
Medo <martinmederly@gmail.com>
2023-12-04 06:31:33 +0100

    feat: day 4 - part 1 and 2

--- day4/day4.go
+++ day4/day4.go
@@ -1,4 +1,81 @@
 package day4
 
+import (
+	"fmt"
+	"github.com/the-medo/go-advent-2023/utils"
+	"strconv"
+	"strings"
+)
+
+type Card struct {
+	winningNumbers []int
+	myNumbers      []int
+	overlapNumbers []int
+	points         int
+	matchingCount  int
+	count          int
+}
+
 func Solve(input string) {
+	cardsSplit := utils.SplitRows(input)
+	cards := make([]*Card, len(cardsSplit))
+
+	for i, _ := range cardsSplit {
+		cards[i] = &Card{
+			count: 1,
+		}
+	}
+
+	for i, card := range cardsSplit {
+		cardSplit := strings.Split(card, ": ")
+		winningAndMyNumbers := strings.Split(cardSplit[1], " | ")
+
+		cards[i].winningNumbers = splitByTwo(winningAndMyNumbers[0])
+		cards[i].myNumbers = splitByTwo(winningAndMyNumbers[1])
+		Overlap(cards[i])
+
+		for j := i + 1; j < cards[i].matchingCount+i+1 && j < len(cardsSplit); j++ {
+			cards[j].count += cards[i].count
+		}
+	}
+
+	cardSum := 0
+	scratchcardCount := 0
+	for _, card := range cards {
+		cardSum += card.points
+		scratchcardCount += card.count
+		fmt.Println("Card", card)
+	}
+
+	fmt.Println("Part 1: ", cardSum)
+	fmt.Println("Part 2: ", scratchcardCount)
+
+}
+
+func Overlap(card *Card) {
+	for _, i1 := range card.winningNumbers {
+		for _, i2 := range card.myNumbers {
+			if i1 == i2 {
+				card.overlapNumbers = append(card.overlapNumbers, i1)
+				if card.points == 0 {
+					card.points = 1
+					card.matchingCount = 1
+				} else {
+					card.points *= 2
+					card.matchingCount++
+				}
+			}
+		}
+	}
+}
+
+func splitByTwo(s string) (result []int) {
+	substrings := strings.Split(s, " ")
+	for _, x := range substrings {
+		if x != "" {
+			a, _ := strconv.Atoi(x)
+			result = append(result, a)
+		}
+	}
+	return
 }

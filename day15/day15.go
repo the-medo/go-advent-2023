7b73a39683005f65a1ef19dd30b0a9dc35d85e07
Medo <martinmederly@gmail.com>
2023-12-15 06:07:23 +0100

    feat: day 15 part 1

--- day15/day15.go
+++ day15/day15.go
@@ -1,4 +1,29 @@
 package day15
 
+import (
+	"fmt"
+	"strings"
+)
+
 func Solve(input string) {
+	inputs := strings.Split(input, ",")
+	total := 0
+	for _, s := range inputs {
+		val := hastString(s)
+		total += val
+		fmt.Println(s, " = ", val, total)
+	}
+
+	fmt.Println("Part 1: ", total)
+}
+
+func hastString(s string) int {
+	currVal := 0
+	for _, c := range s {
+		currVal += int(c)
+		currVal *= 17
+		currVal = currVal % 256
+	}
+
+	return currVal
 }

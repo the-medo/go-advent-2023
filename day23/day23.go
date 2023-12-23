18d1ed062d6b1c812d17c9d0abd109b395b930a4
Medo <martinmederly@gmail.com>
2023-12-23 10:56:26 +0100

    feat: Day 23 part 2

--- day23/day23.go
+++ day23/day23.go
@@ -7,16 +7,28 @@ import (
 
 type Point struct {
 	x, y int
-	max  int
 	t    rune
 }
 
 type Task struct {
-	x, y    int
-	m       *[][]*Point
-	visited map[string]int
-	steps   int
+	currentId int
+	visited   []int
+	steps     int
 }
+
+type Intersection struct {
+	id        int
+	point     *Point
+	neighbors NeighborMap
+}
+
+type Neighbor struct {
+	id             int
+	startX, startY int
+	distance       int
+}
+
+type NeighborMap = map[int]int
 
 func Solve(input string) {
 	rows := utils.SplitRows(input)
@@ -27,7 +39,7 @@ func Solve(input string) {
 	for y, row := range rows {
 		m[y] = make([]*Point, len(row))
 		for x, c := range row {
-			m[y][x] = &Point{x, y, 0, c}
+			m[y][x] = &Point{x, y, c}
 			if y == 0 && c == '.' {
 				start = m[y][x]
 			} else if y == len(rows)-1 && c == '.' {
@@ -37,91 +49,152 @@ func Solve(input string) {
 	}
 	fmt.Println(start, end)
 
-	//visitedCache := []string{k(start.x, start.y)}
-	visitedCache := make(map[string]int)
-
-	queue := []Task{{start.x, start.y, &m, visitedCache, 0}}
-
-	counter := 0
-	maxSteps := 0
-	for len(queue) > 0 {
-		task := queue[0]
-		queue = queue[1:]
-
-		if task.x == end.x && task.y == end.y && task.steps > maxSteps {
-			maxSteps = task.steps
+	for i := 1; i <= 2; i++ {
+		intersections := make(map[int]*Intersection)
+		intersections[1] = &Intersection{
+			id:        1,
+			point:     start,
+			neighbors: make(NeighborMap),
 		}
-		//fmt.Println("Processing task: ", counter, " => ", task)
-		newTasks := task.check()
-		queue = append(queue, newTasks...)
 
-		counter++
-		if counter%1000 == 0 {
-			fmt.Println("i: ", counter, " queue len: ", len(queue))
+		findIntersection(m, intersections, 1, start.x, start.y+1, start.x, start.y, start.x, start.y+1, 1, i, false)
+
+		queue := []Task{{1, make([]int, 0), 0}}
+		counter := 0
+		maxSteps := 0
+		for len(queue) > 0 {
+			task := queue[0]
+			queue = queue[1:]
+
+			taskPoint := intersections[task.currentId].point
+			if taskPoint.x == end.x && taskPoint.y == end.y && task.steps > maxSteps {
+				maxSteps = task.steps
+			}
+			newTasks := task.check(intersections)
+			queue = append(queue, newTasks...)
+
+			counter++
+			if counter%100000 == 0 {
+				fmt.Println("i: ", counter, " queue len: ", len(queue))
+			}
+		}
+
+		// === PART 1 is broken
+		fmt.Println("Part ", i, maxSteps)
+	}
+
+}
+
+func findIntersection(m [][]*Point, i map[int]*Intersection, startIntersectionId int, branchStartX, branchStartY, lastX, lastY, x, y, distance, part int, throughSlope bool) {
+	availablePoints := make([]*Point, 0)
+	//fmt.Println(" =============== ", x, y, " ================= ")
+	if checkPoint(x-1, y, &m, x, y, part) && !(x-1 == lastX && y == lastY) {
+		availablePoints = append(availablePoints, m[y][x-1])
+	}
+	if checkPoint(x+1, y, &m, x, y, part) && !(x+1 == lastX && y == lastY) {
+		availablePoints = append(availablePoints, m[y][x+1])
+	}
+	if checkPoint(x, y-1, &m, x, y, part) && !(x == lastX && y-1 == lastY) {
+		availablePoints = append(availablePoints, m[y-1][x])
+	}
+	if checkPoint(x, y+1, &m, x, y, part) && !(x == lastX && y+1 == lastY) {
+		availablePoints = append(availablePoints, m[y+1][x])
+	}
+
+	if len(availablePoints) == 1 {
+		pt := m[y][x]
+		if part == 1 && (pt.t == '>' || pt.t == 'v') {
+			throughSlope = true
+		}
+		findIntersection(m, i, startIntersectionId, branchStartX, branchStartY, x, y, availablePoints[0].x, availablePoints[0].y, distance+1, part, throughSlope)
+	} else {
+		//fmt.Println(" . ", len(availablePoints), " . ")
+		foundIntersectionId, _, existed := findIntersectionAtPoint(x, y, i)
+
+		i[startIntersectionId].neighbors[foundIntersectionId] = distance
+		if !throughSlope {
+			i[foundIntersectionId].neighbors[startIntersectionId] = distance
+		}
+
+		if !existed {
+			for _, pt := range availablePoints {
+				findIntersection(m, i, foundIntersectionId, pt.x, pt.y, x, y, pt.x, pt.y, 1, part, false)
+			}
+		}
+
+	}
+}
+
+func findIntersectionAtPoint(x, y int, i map[int]*Intersection) (int, *Intersection, bool) {
+	newIntersectionId := len(i) + 1
+	existed := false
+	for k, v := range i {
+		if v.point.x == x && v.point.y == y {
+			newIntersectionId, existed = k, true
+			break
 		}
 	}
 
-	fmt.Println("Part 1: ", maxSteps)
+	var intersection *Intersection
 
+	if existed {
+		intersection = i[newIntersectionId]
+	} else {
+		intersection = &Intersection{
+			id:        newIntersectionId,
+			point:     &Point{x: x, y: y},
+			neighbors: make(NeighborMap),
+		}
+		i[newIntersectionId] = intersection
+	}
+
+	return newIntersectionId, intersection, existed
 }
 
-func (t *Task) check() []Task {
-	newTasks := make([]Task, 0)
-	key := k(t.x, t.y)
-	t.visited[key] = t.steps
-
-	slopeRight := false
-	slopeDown := false
-
-	if (*t.m)[t.y][t.x].t == '>' {
-		slopeRight = true
-	} else if (*t.m)[t.y][t.x].t == 'v' {
-		slopeDown = true
-	}
-
-	if checkPoint(t.x-1, t.y, t.m, t.visited) && !slopeDown && !slopeRight {
-		newTasks = append(newTasks, Task{t.x - 1, t.y, t.m, copyVisited(t.visited), t.steps + 1})
-	}
-	if checkPoint(t.x+1, t.y, t.m, t.visited) && !slopeDown {
-		newTasks = append(newTasks, Task{t.x + 1, t.y, t.m, copyVisited(t.visited), t.steps + 1})
-	}
-	if checkPoint(t.x, t.y-1, t.m, t.visited) && !slopeDown && !slopeRight {
-		newTasks = append(newTasks, Task{t.x, t.y - 1, t.m, copyVisited(t.visited), t.steps + 1})
-	}
-	if checkPoint(t.x, t.y+1, t.m, t.visited) && !slopeRight {
-		newTasks = append(newTasks, Task{t.x, t.y + 1, t.m, copyVisited(t.visited), t.steps + 1})
-	}
-
-	return newTasks
-}
-
-func copyVisited(v map[string]int) map[string]int {
-	r := make(map[string]int)
-	for k, v := range v {
-		r[k] = v
-	}
-	return r
-}
-
-func checkPoint(x, y int, m *[][]*Point, visited map[string]int) bool {
-	//fmt.Print("Checking point ", x, y, " - ")
+func checkPoint(x, y int, m *[][]*Point, lastX, lastY, part int) bool {
 	if x < 0 || x >= len(*m) || y < 0 || y >= len((*m)[0]) {
-		//fmt.Println("False 1")
 		return false
 	}
-	if (*m)[y][x].t == '#' {
-		//fmt.Println("False 2")
+	pt := (*m)[y][x].t
+	lastPt := (*m)[lastY][lastX].t
+	if pt == '#' {
 		return false
 	}
-	_, exists := visited[k(x, y)]
-	if exists {
-		//fmt.Println("False 3")
+	if part == 1 && lastPt == '>' && lastX-x != 1 {
+		fmt.Println("FAIL", string(pt), string(lastPt), x, y, lastX, lastY)
 		return false
 	}
-	//fmt.Println("True")
+	if part == 1 && lastPt == 'v' && lastY-y != 1 {
+		fmt.Println("FAIL", string(pt), string(lastPt), x, y, lastX, lastY)
+		return false
+	}
+
 	return true
 }
 
-func k(n1, n2 int) string {
-	return fmt.Sprintf("%d-%d", n1, n2)
+func (t *Task) check(i map[int]*Intersection) []Task {
+	newTasks := make([]Task, 0)
+
+	visited := make([]int, len(t.visited))
+	copy(visited, t.visited)
+	visited = append(visited, t.currentId)
+
+	for key, distance := range i[t.currentId].neighbors {
+		exists := false
+		for _, e := range t.visited {
+			if e == key {
+				exists = true
+				break
+			}
+		}
+		if !exists {
+			newTasks = append(newTasks, Task{
+				currentId: key,
+				visited:   visited,
+				steps:     t.steps + distance,
+			})
+		}
+	}
+
+	return newTasks
 }
